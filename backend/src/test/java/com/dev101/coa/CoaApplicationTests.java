abb769cbc8cff0d8bcf7814a1a436e8f5a63bb68
이상훈 <dltkdgns0303@gmail.com>
2024-04-22 18:01:29 +0900

    Init: 백엔드 초기파일 생성

---
+++ backend/src/test/java/com/dev101/coa/CoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev101.coa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
