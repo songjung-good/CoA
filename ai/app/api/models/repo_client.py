f34b45f6c802bff95781d1f09a09cac831963746
구본웅 <thesmartist3@gmail.com>
2024-04-23 17:37:05 +0900

    Feat: RepoClient.load() 메소드 생성
    레포 정보와 작성자 이름을 받아 해당 레포에 있는 커밋 변경 내용을 가져오는 메소드

--- ai/app/api/models/repo_client.py
+++ ai/app/api/models/repo_client.py
@@ -1,6 +1,6 @@
 from abc import *
 import requests
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Coroutine
 
 
 class RepoClient(metaclass=ABCMeta):
@@ -9,12 +9,12 @@ class RepoClient(metaclass=ABCMeta):
     """
 
     @staticmethod
-    def __request_json(url: str) -> Any:
+    async def __request_json(url: str) -> Any:
         """
         특정 URL로 JSON 파일을 요청합니다.
 
         Parameters:
-            url(str): 요청을 보낼 URL
+            url: 요청을 보낼 URL
 
         Returns:
             커밋 목록 JSON 객체
@@ -23,16 +23,56 @@ class RepoClient(metaclass=ABCMeta):
         response.raise_for_status()  # Raise an exception for 4XX and 5XX status codes
         return response.json()
 
+    async def load(self, author_name: str) -> list[dict[Any, Any]]:
+        """
+        특정 작성자가 작성한 커밋의 변경 내용을 가져옵니다.
+
+        Parameters:
+            author_name: 커밋 작성자 이름
+
+        Returns:
+
+        """
+        commits = []
+        commits_json = await RepoClient.__request_json(self._get_commits_root_url(author_name))
+        for commit_json in commits_json:
+            commit = {}
+            commit.id = self._get_commit_id_from_commit(commit_json)
+            commit.patches = []
+
+            diff_json = await RepoClient.__request_json(self._get_diff_url_from_commit(commit_json))
+            files_json = self._get_files_from_diff(diff_json)
+            for file_json in files_json:
+                patch = self._get_patch_from_file(file_json)
+                commit.patches.append(patch)
+
+            commits.append(commit)
+
+        return commits
+
     @abstractmethod
     def _get_commits_root_url(self, author_name: str) -> str:
         """
         커밋 목록을 가져올 수 있는 API의 Root URL을 반환합니다.
 
         Parameters:
-            author_name(str):커밋 작성자 이름
+            author_name: 커밋 작성자 이름
 
         Returns:
             커밋 목록을 가져올 수 있는 API의 Root URL
+        """
+        pass
+
+    @abstractmethod
+    def _get_commit_id_from_commit(self, commit_json: Any) -> str:
+        """
+        커밋 목록 JSON의 하나의 커밋에서 커밋 ID를 가져옵니다.
+
+        Parameters:
+            commit_json: 커밋 목록 JSON에서 표현되는 하나의 커밋 JSON
+
+        Returns:
+            커밋 ID
         """
         pass
 
@@ -42,7 +82,7 @@ class RepoClient(metaclass=ABCMeta):
         커밋 목록 JSON의 하나의 커밋에서 커밋 상세 URL을 가져옵니다.
 
         Parameters:
-            commit_json(Any):커밋 목록 JSON에서 표현되는 하나의 커밋 JSON
+            commit_json: 커밋 목록 JSON에서 표현되는 하나의 커밋 JSON
 
         Returns:
             커밋 URL
@@ -50,12 +90,12 @@ class RepoClient(metaclass=ABCMeta):
         pass
 
     @abstractmethod
-    def _get_files_from_diff(self, diff_json: Any) -> List[Any]:
+    def _get_files_from_diff(self, diff_json: Any) -> list[Any]:
         """
         커밋 상세 JSON에서 파일 정보 리스트를 가져옵니다.
 
         Parameters:
-            diff_json:커밋 상세 JSON
+            diff_json: 커밋 상세 JSON
 
         Returns:
             파일 정보 JSON 객체 리스트
@@ -86,7 +126,7 @@ class GithubClient(RepoClient):
         Github 클라이언트를 만듭니다.
 
         Parameters:
-            path(str):Github 저장소 경로 (예: DoubleDeltas/MineCollector)
+            path: Github 저장소 경로 (예: DoubleDeltas/MineCollector)
         """
         self.path = path
 
@@ -95,31 +135,44 @@ class GithubClient(RepoClient):
         커밋 목록을 가져올 수 있는 API의 Root URL을 반환합니다.
 
         Parameters:
-            author_name (str):커밋 작성자 이름
+            author_name: 커밋 작성자 이름
 
         Returns:
             커밋 목록을 가져올 수 있는 API의 Root URL
         """
         return f'https://api.github.com/repos/{self.path}/commits?author={author_name}'
 
+    def _get_commit_id_from_commit(self, commit_json: Any) -> str:
+        """
+        커밋 목록 JSON의 하나의 커밋에서 커밋 ID를 가져옵니다.
+
+        Parameters:
+            commit_json: 커밋 목록 JSON에서 표현되는 하나의 커밋 JSON
+
+        Returns:
+            커밋 ID
+        """
+        return commit_json["sha"]
+
     def _get_diff_url_from_commit(self, commit_json: Any) -> str:
         """
         커밋 목록 JSON의 하나의 커밋에서 커밋 상세 URL을 가져옵니다.
 
         Parameters:
-            commits_json(Any):커밋 목록 JSON에서 표현되는 하나의 커밋 JSON
+            commits_json: 커밋 목록 JSON에서 표현되는 하나의 커밋 JSON
 
         Returns:
             커밋 URL
         """
-        return f'https://api.github.com/repos/{self.path}/commits/{commit_json["sha"]}'
+        commit_id = self._get_commit_id_from_commit(commit_json)
+        return f'https://api.github.com/repos/{self.path}/commits/{commit_id}'
 
     def _get_files_from_diff(self, diff_json: Any) -> List[Any]:
         """
         커밋 상세 JSON에서 파일 정보 리스트를 가져옵니다.
 
         Parameters:
-            diff_json(Any):커밋 상세 JSON
+            diff_json: 커밋 상세 JSON
 
         Returns:
             파일 정보 JSON 객체 리스트
@@ -149,8 +202,8 @@ class GitLabClient(RepoClient):
         GitLab 클라이언트를 만듭니다.
 
         Parameters:
-            base_url(str):GitLab 저장소 기본 URL (예: https://github.example.com)
-            project_id(int):Project ID
+            base_url: GitLab 저장소 기본 URL (예: https://github.example.com)
+            project_id: GitLab Project ID
         """
         self.base_url = base_url
         self.project_id = project_id
@@ -160,31 +213,44 @@ class GitLabClient(RepoClient):
         커밋 목록을 가져올 수 있는 API의 Root URL을 반환합니다.
 
         Parameters:
-            author_name (str):커밋 작성자 이름
+            author_name: 커밋 작성자 이름
 
         Returns:
             커밋 목록을 가져올 수 있는 API의 Root URL
         """
         return f'{self.base_url}/api/v4/projects/{self.project_id}/repository/commits?author={author_name}'
 
+    def _get_commit_id_from_commit(self, commit_json: Any) -> str:
+        """
+        커밋 목록 JSON의 하나의 커밋에서 커밋 ID를 가져옵니다.
+
+        Parameters:
+            commit_json: 커밋 목록 JSON에서 표현되는 하나의 커밋 JSON
+
+        Returns:
+            커밋 ID
+        """
+        return commit_json["id"]
+
     def _get_diff_url_from_commit(self, commit_json: Any) -> str:
         """
         커밋 목록 JSON의 하나의 커밋에서 커밋 상세 URL을 가져옵니다.
 
         Parameters:
-            commit_json(Any):커밋 목록 JSON에서 표현되는 하나의 커밋 JSON
+            commit_json: 커밋 목록 JSON에서 표현되는 하나의 커밋 JSON
 
         Returns:
             커밋 URL
         """
-        return f'{self.base_url}/api/v4/projects/{self.project_id}/repository/commits/{commit_json["id"]}/diff'
+        commit_id = self._get_commit_id_from_commit(commit_json)
+        return f'{self.base_url}/api/v4/projects/{self.project_id}/repository/commits/{commit_id}/diff'
 
     def _get_files_from_diff(self, diff_json: Any) -> List[Any]:
         """
         커밋 상세 JSON에서 파일 정보 리스트를 가져옵니다.
 
         Parameters:
-            diff_json(Any):커밋 상세 JSON
+            diff_json: 커밋 상세 JSON
 
         Returns:
             파일 정보 JSON 객체 리스트
