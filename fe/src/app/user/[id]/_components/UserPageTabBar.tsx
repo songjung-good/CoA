c25fadae6bdcf22fc8491311eafd4f1ba5a28301
신현중 <qsc3117@naver.com>
2024-04-23 17:33:54 +0900

    Feat: UserPage use "use client"

--- fe/src/app/user/[id]/_components/UserPageTabBar.tsx
+++ fe/src/app/user/[id]/_components/UserPageTabBar.tsx
@@ -1,6 +1,3 @@
-"use client";
-import Link from "next/link";
-
 export default function UserPageTabBar() {
   const navButtonCSS =
     "px-6 py-3 rounded-lg bg-appGrey2 hover:bg-appBlue1 whitespace-nowrap";
@@ -11,26 +8,10 @@ export default function UserPageTabBar() {
       flex justify-between overflow-auto"
     >
       <div className="flex gap-4 pr-4">
-        <button
-          className={`${navButtonCSS} bg-appBlue2`}
-        >
-          개요
-        </button>
-        <button
-          className={`${navButtonCSS} `}
-        >
-          연혁
-        </button>
-        <button
-          className={`${navButtonCSS}`}
-        >
-          레포
-        </button>
-        <button
-          className={`${navButtonCSS}`}
-        >
-          심층분석
-        </button>
+        <button className={`${navButtonCSS} bg-appBlue2`}>개요</button>
+        <button className={`${navButtonCSS} `}>연혁</button>
+        <button className={`${navButtonCSS}`}>레포</button>
+        <button className={`${navButtonCSS}`}>심층분석</button>
       </div>
       <button
         className={`${navButtonCSS} bg-gradient-to-r from-[#F5E5CA] to-[#41EAE5]`}
--- fe/src/app/user/[id]/page.tsx
+++ fe/src/app/user/[id]/page.tsx
@@ -1,10 +1,16 @@
-export default function UserPage({
-  params,
-}: {
-  params: { id: string };
-}) {
+"use client";
+
+import { useState } from "react";
+import UserPageTabBar from "./_components/UserPageTabBar";
+
+export default function UserPage({ params }: { params: { id: string } }) {
+  const [tabIndex, setTabIndex] = useState(0);
+  const onClickTap = (idx: number) => {
+    setTabIndex(idx);
+  };
   return (
     <>
+      <UserPageTabBar />
       <h1>User Page ID: {params.id}</h1>
       <h1>개요</h1>
     </>
