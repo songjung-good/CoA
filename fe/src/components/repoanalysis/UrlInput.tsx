d29c5ac35b6d5783c926645ca29f98e8f4bed395
YH <139380475+songjung-good@users.noreply.github.com>
2024-04-23 17:30:07 +0900

    Refactor: 타입지정
    1. 함수에 필요한 값들의 타입을 지정하였습니다.
    2. axios요청으로 수정
    3. enter키로도 입력 가능

--- fe/src/components/repoanalysis/UrlInput.tsx
+++ fe/src/components/repoanalysis/UrlInput.tsx
@@ -2,9 +2,10 @@
 
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
 
 // GitHub 정보 추출 함수
-const extractUserInfo = (url) => {
+const extractUserInfo = (url: string) => {
   const regex = /https:\/\/github\.com\/([^\/]+)\/([^\/]+)/;
   const match = url.match(regex);
 
@@ -19,32 +20,54 @@ const UrlInput = () => {
   const [inputValue, setInputValue] = useState('');
 
   // 입력 값 변경 시 핸들러
-  const handleChange = (event) => {
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(event.target.value);
   };
 
-  // 정보 추출 예시 (추출 로직은 실제 애플리케이션의 다른 부분에서 활용될 수 있음)
-  const githubInfo = extractUserInfo(inputValue);
-  // 콘솔에 사용자 이름과 레포지토리 명 출력
-  console.log(githubInfo); 
+  // Enter 키 입력 시 실행될 함수
+  const handleKeyDown = (event: React.KeyboardEvent) => {
+    if (event.key === 'Enter') {
+      fetchGitHubInfo(); // Enter 키를 누르면 분석을 시작합니다.
+    }
+  };
+
+  // GitHub 정보 요청 함수
+  const fetchGitHubInfo = async () => {
+    const githubInfo = extractUserInfo(inputValue);
+
+    if (githubInfo.username && githubInfo.repositoryName) {
+      try {
+        const response = await axios.get(`https://api.github.com/repos/${githubInfo.username}/${githubInfo.repositoryName}/contributors`);
+        console.log(response.data); // 응답 데이터를 콘솔에 출력. 실제 애플리케이션에서는 이 데이터를 적절하게 활용합니다.
+      } catch (error) {
+        console.error("GitHub 정보를 가져오는 데 실패했습니다.", error);
+      }
+    } else {
+      console.log("유효한 GitHub URL을 입력하세요.");
+    }
+  };
 
   return (
-    <div>
+    <Styleddiv>
       <StyledInput
         type="text"
         placeholder="🔎Repository URL을 입력하세요"
         value={inputValue}
         onChange={handleChange}
+        onKeyDown={handleKeyDown}
       />
-      <button>
-        <a href={`https://api.github.com/repos/${githubInfo.username}/${githubInfo.repositoryName}/contributors`}>
-          분석하기
-        </a>
-      </button>
-    </div>
+      <Styledbutton onClick={fetchGitHubInfo}>분석하기</Styledbutton>
+    </Styleddiv>
   );
 };
-  
+
+// 
+const Styleddiv = styled.div`
+  display: flex; 
+  alignItems: center;
+`;
+
+// 입력창 css
 const StyledInput = styled.input`
   width: 80%;
   padding: 10px;
@@ -53,9 +76,17 @@ const StyledInput = styled.input`
   border: 2px solid black;
   border-radius: 25px;
   transition: border-color 0.3s ease;
-
   &:hover {
-    border-color: #333;
+    border-color: #cccccc;
+  }
+`;
+
+// 입력 버튼 css
+const Styledbutton = styled.button`
+  border: 2px solid black;
+  border-radius: 25px;
+  &:hover {
+    border-color: #cccccc;
   }
 `;
 
