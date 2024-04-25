32bf8a19b81a078b6aab0de46e907d7b44136327
신현중 <qsc3117@naver.com>
2024-04-25 11:41:41 +0900

    Feat: data년별로 분류, svg size 조정

--- fe/src/app/user/[id]/_components/CalendarChart.tsx
+++ fe/src/app/user/[id]/_components/CalendarChart.tsx
@@ -6,24 +6,51 @@ import {
 } from "@/api/userPage/apiContributions";
 
 const CalendarChart: React.FC = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
   // github에서 contributions(잔디) 가져오기
+  const [totalContribution, setTotalContribution] = useState<
+    Record<string, number>
+  >({ "2024": 200 });
+  const [dataByYear, SetDataByYear] = useState<Record<string, Contribution[]>>(
+    {},
+  );
   const [data, setData] = useState<Contribution[]>([]);
   useEffect(() => {
     const fetchData = async () => {
       const res = await getContributions();
-      setData(res.contributions);
+      setTotalContribution(res.total);
+
+      // data를 년도별로 분류
+      const dataByYear = res.contributions.reduce(
+        (acc, contribution) => {
+          const year = new Date(contribution.date).getFullYear();
+          if (!acc[year]) {
+            acc[year] = [];
+          }
+          acc[year].push(contribution);
+          return acc;
+        },
+        {} as Record<string, Contribution[]>,
+      );
+      SetDataByYear(dataByYear);
+      const firstKey = Object.keys(dataByYear)[0];
+      setData(dataByYear[firstKey]);
+
+      setIsLoading(false);
     };
     fetchData();
   }, []);
-  const svgRef = useRef<SVGSVGElement>(null);
 
+  // Chart svg 만들기
+  const svgRef = useRef<SVGSVGElement>(null);
   useEffect(() => {
     if (!svgRef.current) return;
 
     const svg = d3.select(svgRef.current);
-
-    const cellSize = 16;
-    const width = cellSize * 52;
+    const svgWidth = svgRef.current.getBoundingClientRect().width;
+    const cellSize = svgWidth / 53;
+    const width = cellSize * 53;
     const height = cellSize * 7;
     const colorRange: string[] = [
       "#ebedf0",
@@ -32,7 +59,6 @@ const CalendarChart: React.FC = () => {
       "#30a14e",
       "#216e39",
     ];
-    // const color = d3.scaleQuantize().domain([0, 4]).range(colorRange);
     const color = d3.scaleQuantize([0, 4], colorRange);
 
     const dateExtent = d3.extent(data, (d) => new Date(d.date));
@@ -59,7 +85,25 @@ const CalendarChart: React.FC = () => {
       .attr("stroke", "#fff");
   }, [data]);
 
-  return <svg ref={svgRef}></svg>;
+  return isLoading ? (
+    <p>Loading</p>
+  ) : (
+    <section className="m-4 p-4 bg-appGrey1 rounded-2xl">
+      <div className="flex gap-2 items-center">
+        <p className="text-lg">
+          total: {Object.values(totalContribution).reduce((a, b) => a + b)}
+        </p>
+        <ul className="flex gap-2">
+          {Object.entries(totalContribution).map(([key, value]) => (
+            <li key={key} className="px-4 py-2 bg-appGrey2 rounded-md">
+              {key}: {value}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <svg className="w-full" ref={svgRef}></svg>
+    </section>
+  );
 };
 
 export default CalendarChart;
