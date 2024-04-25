2ecd6f89bd36b981ac443a717cbe0d108374b03c
김동영 <ehddud0534@naver.com>
2024-04-25 17:33:38 +0900

    hotfix: next.config 파일 수정
    .ts, .tsx 알아서 찾아가게 해봤습니다

--- fe/next.config.mjs
+++ fe/next.config.mjs
@@ -1,6 +1,21 @@
+// next.config.js
+import path from 'path';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config, options) => {
+    config.resolve.extensions.push('.ts', '.tsx');
+    return config;
+  },
   output: "standalone",
 };
 
 export default nextConfig;
+
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: "standalone",
+// };
+
+// export default nextConfig;
