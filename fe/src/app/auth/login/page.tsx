c4f5f83722917b0ac6b6c5e3cc83f9163f9801b0
신현중 <qsc3117@naver.com>
2024-04-26 18:00:25 +0900

    Design: LoginPage

--- fe/src/app/auth/login/page.tsx
+++ fe/src/app/auth/login/page.tsx
@@ -1,13 +1,12 @@
-import { SignIn } from "@/components/SignIn";
+import Image from "next/image";
 
 export default function LoginPage() {
   return (
-    <main>
-      <h1>LoginPage</h1>
-      <div className="felx felx-col">
+    <main className="flex justify-center items-center">
+      <div className="flex flex-col gap-4 justify-center items-center shadow-lg p-8">
+        <Image src="/image/logo144.png" alt="logo" width={144} height={144} />
+        <h1>시작하기</h1>
         <button>github</button>
-        <SignIn />
-        <p></p>
         <button>google</button>
       </div>
     </main>
