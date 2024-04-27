199fd74d1602d5ef8e9b0f319009b9ecbc67edcf
이상훈 <dltkdgns0303@gmail.com>
2024-04-27 15:48:50 +0900

    Feat: 로그인 로직 작성

--- fe/src/app/auth/login/page.tsx
+++ fe/src/app/auth/login/page.tsx
@@ -1,14 +1,42 @@
-import { SignIn } from "@/components/SignIn";
+'use client'
+
+
+function generateState(): string {
+  const array = new Uint32Array(1);
+  window.crypto.getRandomValues(array);
+  const state = array[0].toString(16);
+  sessionStorage.setItem('oauth_state', state);
+  return state;
+}
+
 
 export default function LoginPage() {
+
+  const handleGoogleLogin = () => {
+
+    const state = generateState();
+
+    const clientId = '765601865422-gfbpej2d4oequfvi35v14j6cba5iafvr.apps.googleusercontent.com';
+    const redirectUri = 'http://localhost:8080/login/oauth2/code/google';
+    const scope = 'email profile';
+    const responseType = 'code';
+    const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}&response_type=${responseType}&state=${state}`;
+
+    window.location.href = authUrl;
+  };
+
+  // GitHub 로그인 버튼 클릭 시 이벤트 핸들러
+  const handleGitHubLogin = () => {
+  };
+
+
   return (
     <main>
       <h1>LoginPage</h1>
-      <div className="felx felx-col">
-        <button>github</button>
-        <SignIn />
+      <div className="flex flex-col">
+        <button onClick={handleGitHubLogin}>GitHub</button>
         <p></p>
-        <button>google</button>
+        <button onClick={handleGoogleLogin}>Sign In Google</button>
       </div>
     </main>
   );
