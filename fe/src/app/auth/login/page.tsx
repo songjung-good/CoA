962a0a14bba4e408f87cc450aafe56744a745c00
이상훈 <dltkdgns0303@gmail.com>
2024-04-29 23:55:58 +0900

    Feat: 구글 로그인 요청 완

--- fe/src/app/auth/login/page.tsx
+++ fe/src/app/auth/login/page.tsx
@@ -1,39 +1,9 @@
 'use client'
 
-
-function generateState(): string {
-  const array = new Uint32Array(1);
-  window.crypto.getRandomValues(array);
-  const state = array[0].toString(16);
-  sessionStorage.setItem('oauth_state', state);
-  return state;
-}
-
-
 export default function LoginPage() {
 
-  // const handleGoogleLogin = () => {
-
-  //   const state = generateState();
-
-  //   const clientId = '765601865422-gfbpej2d4oequfvi35v14j6cba5iafvr.apps.googleusercontent.com';
-  //   const redirectUri = 'http://localhost:8080/login/oauth2/code/google';
-  //   const scope = 'email profile';
-  //   const responseType = 'code';
-  //   const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}&response_type=${responseType}&state=${state}`;
-  //   // const authUrl = `https://accounts.google.com/o/oauth2/v2/authorization?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}&response_type=${responseType}&state=${state}`;
-
-  //   window.location.href = authUrl;
-  // };
-
-
   const handleGoogleLogin = () => {
-    fetch('http://localhost:8080/api/auth/google/start')
-      .then(response => response.json())
-      .then(data => {
-        window.location.href = data.url;
-      })
-      .catch(error => console.error('Error:', error));
+    window.location.href = 'http://localhost:8080/oauth2/authorization/google'; // OAuth는 fetch나 axios로 보낼 시 무조건 CORS에 막히는 듯
   };
 
   // GitHub 로그인 버튼 클릭 시 이벤트 핸들러
