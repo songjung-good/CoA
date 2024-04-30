45b52c02ba7f03e7eb0907a4fa16488c94a3df38
신현중 <qsc3117@naver.com>
2024-04-30 15:51:00 +0900

    Feat: History Card

--- fe/src/app/user/[id]/_components/HistoryChart.tsx
+++ fe/src/app/user/[id]/_components/HistoryChart.tsx
@@ -1,9 +1,48 @@
-import { Repository } from "@/api/userPage/apiLinesOfCode";
+import { useEffect, useRef } from "react";
+import * as d3 from "d3";
+import repositoryStore from "@/store/repos";
 
-export default function HistoryChart(data: Repository) {
-  return (
-    <>
-      <p>HistoryChart</p>
-    </>
-  );
+export default function HistoryChart() {
+  const repos = repositoryStore((state) => state.repos);
+  const svgRef = useRef<SVGSVGElement>(null);
+
+  useEffect(() => {
+    if (!svgRef.current || repos.length === 0) return;
+
+    const svg = d3.select(svgRef.current);
+    const margin = { top: 20, right: 30, bottom: 30, left: 40 };
+    const width = 960 - margin.left - margin.right;
+    const height = 500 - margin.top - margin.bottom;
+
+    const x = d3.scaleTime().range([0, width]);
+    const y = d3.scaleBand().range([height, 0]);
+
+    const g = svg
+      .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    g.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
+
+    g.append("g").call(d3.axisLeft(y));
+
+    g.selectAll(".bar")
+      .data(repos)
+      .enter()
+      .append("rect")
+      .attr("class", "bar")
+      .attr("x", (d) => x(new Date(d.createdAt)))
+      .attr("y", 0)
+      .attr("width", (d) => {
+        const startDate = new Date(d.createdAt);
+        const endDate = new Date(d.pushedAt);
+        return isNaN(startDate.getTime()) || isNaN(endDate.getTime())
+          ? 0
+          : x(endDate) - x(startDate);
+      })
+      .attr("height", y.bandwidth());
+  }, [repos]);
+
+  return <svg ref={svgRef} width="960" height="500" />;
 }
