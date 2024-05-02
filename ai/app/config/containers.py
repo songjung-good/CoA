6004132e1968170dd10b1f6052a7333f39853001
구본웅 <thesmartist3@gmail.com>
2024-05-02 09:09:24 +0900

    Feat: Redis IoC 연결

--- ai/app/config/containers.py
+++ ai/app/config/containers.py
@@ -1,11 +1,16 @@
 from dependency_injector.containers import DeclarativeContainer
 from dependency_injector import providers
 
+from dotenv import load_dotenv
 from redis import Redis
 
 
 class Container(DeclarativeContainer):
     config = providers.Configuration()
+
+    load_dotenv()
+    config.redis.host.from_env('REDIS_HOST', as_=str, default='')
+    config.redis.port.from_env('REDIS_PORT', as_=int, default=0)
 
     redis_client = providers.Resource(
         provides=Redis,
