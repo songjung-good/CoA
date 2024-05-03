6cde99aac24d0dba5389b197df3a23d4ed68349d
김동영 <139415938+SSAFY10kim@users.noreply.github.com>
2024-05-03 12:01:39 +0900

    feat: MD Editor 적용

--- fe/src/app/result/[id]/_components/ResultReadme.tsx
+++ fe/src/app/result/[id]/_components/ResultReadme.tsx
@@ -1,7 +1,29 @@
-export default function ResultReadme() {
+import { useState, useEffect } from "react";
+import useResultStore from "@/store/result";
+
+// mdEditor-------------------------------------------
+import type { NextPage } from "next";
+import "@uiw/react-md-editor/markdown-editor.css";
+import "@uiw/react-markdown-preview/markdown.css";
+import dynamic from "next/dynamic";
+// mdEditor-------------------------------------------
+
+const MDEditor = dynamic(() => import("@uiw/react-md-editor"), {
+  ssr: false,
+});
+const ResultReadme: NextPage = () => {
+  const [md, setMd] = useState<string | undefined>("# Hello World");
+  const { isOwn, setIsOther, setIsOwn } = useResultStore((state) => state);
+
   return (
-    <div className="flex justify-center w-3/4 p-4 bg-white shadow-lg rounded-2xl">
-      <div className="">리드미</div>
+    <div className="flex flex-col justify-center items-center w-3/4 p-4 bg-white shadow-lg rounded-2xl">
+      <MDEditor value={md} onChange={setMd} />
+      <div className=" ">
+        <button onClick={setIsOwn}>본인</button>
+        <button onClick={setIsOther}>타인</button>
+      </div>
     </div>
   );
-}
+};
+
+export default ResultReadme;
--- fe/src/app/result/[id]/page.tsx
+++ fe/src/app/result/[id]/page.tsx
@@ -1,11 +1,15 @@
+"use client";
+
 import RepoInfo from "@/components/result/RepoInfo.tsx";
 import ResultTap from "@/components/result/ResultTap.tsx";
+import useResultStore from "@/store/result";
 
 export default function ResultPage({ params }: { params: { id: string } }) {
+  const { isOwn } = useResultStore((state) => state);   // 이후 유저닉네임으로 변경
   return (
     <div className="flex flex-col items-center bg-appGrey1 pt-5 p-10 w-full h-full">
       <p className="mb-5 text-2xl font-bold">
-        OOO님의 레포지토리 분석 결과입니다.
+        {`${isOwn}님의 레포지토리 분석 결과입니다.`}
       </p>
       <RepoInfo />
       <ResultTap />
