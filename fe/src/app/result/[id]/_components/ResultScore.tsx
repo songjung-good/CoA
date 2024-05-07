a29c79b01691a9967710c005805e106541eed18d
김동영 <139415938+SSAFY10kim@users.noreply.github.com>
2024-05-07 15:30:04 +0900

    feat: repoScore chart.js 추가

--- fe/src/app/result/[id]/_components/ResultScore.tsx
+++ fe/src/app/result/[id]/_components/ResultScore.tsx
@@ -1,7 +1,14 @@
+import useResultStore from "@/store/result";
+import RepoScore from "./RepoScore";
+
 export default function ResultScore() {
+  const codeScore = useResultStore.getState().result.commitScoreDto;
+
   return (
     <div className="flex flex-col items-center w-full">
-      <div className="min-h-60 mb-10">레포 점수 그래프</div>
+      <div className="min-h-60 mb-10">
+        <RepoScore />
+      </div>
       <div className="bg-white mb-10 shadow-lg rounded-2xl w-full flex justify-center min-h-32">
         레포점수
       </div>
