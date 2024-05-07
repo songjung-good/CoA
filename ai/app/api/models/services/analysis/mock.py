7214146dbee0f5cf7b085ca09892afe47f3a51bf
구본웅 <thesmartist3@gmail.com>
2024-05-07 11:48:06 +0900

    Fix: MockAnalysisService asyncio import 수정

--- ai/app/api/models/services/analysis/mock.py
+++ ai/app/api/models/services/analysis/mock.py
@@ -1,16 +1,15 @@
+import asyncio
 from typing import Generic, TypeVar, Any
-
-from tqdm import asyncio
 
 from api.models.dto import AnalysisRequest
 from api.models.services.analysis import AnalysisService
 
-
 R = TypeVar('R', bound=AnalysisRequest, covariant=True)
+
 
 class MockAnalysisService(Generic[R], AnalysisService[R]):
     """가짜 분석 서비스
-    데이터 요청(5초) - 학습(5초) - 답변 생성(10초)의 딜레이로 고정된 데이터를 보내줍니다.
+    일정한 딜레이로 고정된 데이터를 보내줍니다.
     """
 
     async def assert_content_loadable(self) -> None:
@@ -31,3 +30,6 @@ class MockAnalysisService(Generic[R], AnalysisService[R]):
                 'patches': ['@@ -0,0 +1,2 @@\n+# CoATest\n+test repo']
             }
         ]
+
+    async def load_content(self, request: R) -> list[dict[Any, Any]]:
+        pass
