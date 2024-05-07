b37acf6a2208d8245ee32b08b943242ba323e0a8
김하영 <ha09368@naver.com>
2024-05-07 16:31:33 +0900

    Refactor: redisTemplate 방식에서 Redis repository 방식으로 변경

---
+++ backend/src/main/java/com/dev101/coa/domain/redis/RedisResult.java
@@ -0,0 +1,33 @@
+package com.dev101.coa.domain.redis;
+
+import com.dev101.coa.domain.repo.dto.AiResultDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
+
+import java.time.LocalDate;
+
+@RedisHash("result")
+@AllArgsConstructor
+@Builder
+@Getter
+public class RedisResult {
+    @Id
+    private String analysisId;
+    private String repoPath;
+    private String projectId;
+    private String userName;
+    private Long memberId;
+    private Boolean isOwn;
+    private Integer percentage;
+    private LocalDate repoStartDate;
+    private LocalDate repoEndDate;
+    private Integer repoMemberCnt;
+    private AiResultDto result;
+    @TimeToLive
+    private Long expireSec;
+
+}
