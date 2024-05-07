fcd08da574f3e11db400112f3f3996a284ed2039
신현중 <qsc3117@naver.com>
2024-05-07 11:21:52 +0900

    Style: prettier (변경사항없음)

--- fe/src/store/user.ts
+++ fe/src/store/user.ts
@@ -3,11 +3,19 @@ import { create } from "zustand";
 interface UserStore {
   githubUserName: string;
   setGithubUserName: (input: string) => void;
+  gitlabUserName: string;
+  setGitlabUserName: (input: string) => void;
+  AuthUserName: string;
+  setAuthUserName: (input: string) => void;
 }
 
 const userStore = create<UserStore>((set) => ({
   githubUserName: "Shin-3117",
   setGithubUserName: (input: string) => set({ githubUserName: input }),
+  gitlabUserName: "qsc3117",
+  setGitlabUserName: (input: string) => set({ gitlabUserName: input }),
+  AuthUserName: "auth username",
+  setAuthUserName: (input: string) => set({ AuthUserName: input }),
 }));
 
 export default userStore;
