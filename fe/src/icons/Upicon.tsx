8d2bc24747fe35e8c2eacac919eeacfdc50021f7
신현중 <qsc3117@naver.com>
2024-05-09 11:16:00 +0900

    Fix: Icon 속성 error

--- fe/src/icons/Upicon.tsx
+++ fe/src/icons/Upicon.tsx
@@ -11,13 +11,13 @@ export default function Upicon({ width, height }: UpiconProps) {
       width={width}
       height={height}
       viewBox="0 0 24 24"
-      stroke-width="1.5"
+      strokeWidth="1.5"
       stroke="currentColor"
       className="w-6 h-6"
     >
       <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         d="m4.5 15.75 7.5-7.5 7.5 7.5"
       />
     </svg>
