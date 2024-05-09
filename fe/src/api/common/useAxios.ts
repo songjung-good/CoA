c370c12868ce145ab0f3290ecdedb95002d1217a
이상훈 <dltkdgns0303@gmail.com>
2024-05-09 15:42:28 +0900

    Fix:

--- fe/src/api/common/useAxios.ts
+++ fe/src/api/common/useAxios.ts
@@ -2,13 +2,13 @@
 import axios, { AxiosInstance } from "axios";
 
 const UseAxios = (): AxiosInstance => {
-  
+
   function getDomainAddress() {
     if (typeof window === "undefined") {
       // 서버 사이드에서 실행되는 경우
       return "http://localhost:8080"; // 또는 다른 기본 URL
     }
-    var hostname = window.location.hostname; // 현재 도메인 이름을 가져옵니다.
+    const hostname = window.location.hostname; // 현재 도메인 이름을 가져옵니다.
     if (hostname === "localhost") {
         return "http://localhost:8080";
     } else {
