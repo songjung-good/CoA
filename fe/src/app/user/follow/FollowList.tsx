1d2abb03b338127b4f065688f621fcfd7a11038c
신현중 <qsc3117@naver.com>
2024-05-10 17:33:39 +0900

    Feat: Follow POST 요청

--- fe/src/app/user/follow/FollowList.tsx
+++ fe/src/app/user/follow/FollowList.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import UseAxios from "@/api/common/useAxios";
 import { colorMapping } from "../[id]/_components/colorMap";
 import IsStar from "@/components/usercard/IsStar";
+import useCommonCodeStore from "@/store/commoncode";
 
 interface Skill {
   codeId: number;
@@ -11,7 +12,7 @@ interface Skill {
 }
 
 interface Member {
-  memberUuid: number;
+  memberUuid: string;
   memberNickName: string;
   memberImg: string;
   memberIntro: string;
@@ -27,7 +28,7 @@ interface ApiResponse {
 
 const dummyData: Member[] = [
   {
-    memberUuid: 1,
+    memberUuid: "노마드 코더",
     memberNickName: "노마드 코더",
     memberImg:
       "https://yt3.googleusercontent.com/ytc/AIdro_kZGbEvWmB_2CZMcZVcCpjFsiQNVQZEehF8jinP6zlFJ7s=s176-c-k-c0x00ffffff-no-rj",
@@ -41,7 +42,7 @@ const dummyData: Member[] = [
     isBookmark: true,
   },
   {
-    memberUuid: 2,
+    memberUuid: "개발바닥",
     memberNickName: "개발바닥",
     memberImg:
       "https://yt3.googleusercontent.com/ytc/AIdro_mDWXwLF9kj8Hzm_nh3ZDVo0LAzH-DzXyaFa8odzPeBTw=s176-c-k-c0x00ffffff-no-rj",
@@ -57,7 +58,7 @@ const dummyData: Member[] = [
 ];
 const noFollowData = [
   {
-    memberUuid: 0,
+    memberUuid: "no-follow-user",
     memberNickName: "팔로우한 유저가 없어요!",
     memberImg: "/image/logo200.png",
     memberIntro: `다른 유저를 팔로우 해보세요!`,
@@ -70,16 +71,18 @@ const noFollowData = [
 export default function FollowList() {
   const [isLoading, setIsLoading] = useState(true);
   const [data, setData] = useState<Member[]>([]);
+  const { response } = useCommonCodeStore.getState();
+
   const fetchFollowData = async () => {
     try {
-      const response = await UseAxios().get<ApiResponse>(
+      const bookmarksResponse = await UseAxios().get<ApiResponse>(
         "/api/member/bookmarks",
       );
-      console.log(response.data);
-      if (response.data.result.length === 0) {
+      // console.log(response.data);
+      if (bookmarksResponse.data.result.length === 0) {
         setData(noFollowData);
       } else {
-        setData(response.data.result);
+        setData(bookmarksResponse.data.result);
       }
       //더미데이터 사용 임시
       // setData(dummyData);
@@ -88,8 +91,11 @@ export default function FollowList() {
       console.error("'/api/member/bookmarks'요청 에러", error);
     }
   };
+
   useEffect(() => {
     fetchFollowData();
+    console.log(response.result);
+    console.log(response.result.commonCodeList[2].codes);
   }, []);
 
   return (
@@ -103,16 +109,29 @@ export default function FollowList() {
           {data.map((member) => (
             <li key={member.memberUuid} className="card flex flex-col gap-4">
               <div className="flex flex-row gap-4">
-                <img
-                  src={member.memberImg}
-                  alt={member.memberNickName}
-                  style={{ width: "144px", height: "144px" }}
-                />
-
+                <div className="rounded-full overflow-hidden">
+                  <img
+                    src={member.memberImg}
+                    alt={member.memberNickName}
+                    style={{ width: "144px", height: "144px" }}
+                  />
+                </div>
                 <div className="flex flex-col grow gap-2">
                   <div className="flex justify-between">
                     <p>{member.memberNickName}</p>
-                    <IsStar />
+                    <div className="flex gap-2">
+                      <p>
+                        {
+                          response.result.commonCodeList[2].codes![
+                            `${member.memberJobCodeId}`
+                          ]
+                        }
+                      </p>
+                      <IsStar
+                        isBookmark={member.isBookmark}
+                        Uuid={member.memberUuid}
+                      />
+                    </div>
                   </div>
                   <div className="bg-appGrey1 p-4 rounded-2xl grow">
                     <p>{member.memberIntro}</p>
--- fe/src/components/usercard/IsStar.tsx
+++ fe/src/components/usercard/IsStar.tsx
@@ -1,15 +1,37 @@
 "use client";
 
+import UseAxios from "@/api/common/useAxios";
+import { ApiResponse } from "@/api/userPage/apiContributions";
 import StartIcon from "@/icons/StarIcon";
 import StartIconFilled from "@/icons/StarIconFilled";
 import { useState } from "react";
 
-export default function IsStar() {
-  const [starState, setStartState] = useState(false);
+export default function IsStar({
+  isBookmark,
+  Uuid,
+}: {
+  isBookmark: boolean;
+  Uuid: string;
+}) {
+  const [starState, setStartState] = useState(isBookmark);
 
-  const changeStar = () => {
-    setStartState(!starState);
+  const postFollowData = async () => {
+    try {
+      const postResponse = await UseAxios().post(
+        `/api/member/bookmarks/${Uuid}`,
+      );
+      console.log(postResponse.data.result["currentStatus"]);
+      return postResponse.data.result["currentStatus"];
+    } catch (error) {
+      console.error("'팔로우 post'요청 에러", error);
+      return starState;
+    }
+  };
+  const changeStar = async () => {
+    // setStartState(!starState);
     //팔로우 변경시, post 요청 추가하기
+    const response = await postFollowData();
+    setStartState(response);
   };
 
   return (
