6c3005be60deea6e865b5d7de8b11ffa46b89dbd
신현중 <qsc3117@naver.com>
2024-05-10 16:55:00 +0900

    Design: Header 수정

--- fe/src/icons/SearchIcon.tsx
+++ fe/src/icons/SearchIcon.tsx
@@ -6,7 +6,7 @@ export default function SearchIcon() {
       viewBox="0 0 24 24"
       strokeWidth={1.5}
       stroke="currentColor"
-      className="w-6 h-6"
+      className="w-10 h-10"
     >
       <path
         strokeLinecap="round"
