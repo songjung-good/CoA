f543029ced32cf7796d3a9215176e91ad3daae2e
구본웅 <thesmartist3@gmail.com>
2024-05-12 21:17:42 +0900

    Refactor: OpenAI 모델로 교체

--- ai/app/config/containers.py
+++ ai/app/config/containers.py
@@ -4,9 +4,8 @@ from typing import TypeVar
 from dependency_injector import providers
 from dependency_injector.containers import DeclarativeContainer
 from dotenv import load_dotenv
-from langchain_community.llms.llamacpp import LlamaCpp
-from langchain_core.language_models import BaseLLM, BaseChatModel
-from langchain_experimental.chat_models import Llama2Chat
+from langchain_openai import ChatOpenAI
+from langchain_openai import OpenAI
 from redis import Redis
 
 from api.models.dto import GithubAnalysisRequest, GitLabAnalysisRequest, AnalysisRequest
@@ -27,7 +26,8 @@ class Container(DeclarativeContainer):
     config.redis.host.from_env('REDIS_HOST', as_=str, required=True)
     config.redis.port.from_env('REDIS_PORT', as_=int, required=True)
 
-    config.ai.model_path.from_env('MODEL_PATH', as_=str, required=True)
+    # config.ai.model_path.from_env('MODEL_PATH', as_=str, required=True)
+    config.ai.openai_api_key.from_env('OPENAI_API_KEY', as_=str, required=True)
 
     redis_client = providers.Resource(
         provides=Redis,
@@ -40,18 +40,29 @@ class Container(DeclarativeContainer):
         GitLabAnalysisRequest: providers.Factory(GitLabClient)
     }
 
+    # llm = providers.Resource(
+    #     provides=LlamaCpp,
+    #     model_path=config.ai.model_path,
+    #     n_gpu_layers=-1,
+    #     temperature=0.3,
+    #     f16_kv=True,
+    #     verbose=False
+    # )
+    # chat_model = providers.Resource(
+    #     provides=Llama2Chat,
+    #     llm=llm,
+    #     temparature=0.3
+    # )
+
     llm = providers.Resource(
-        provides=LlamaCpp,
-        model_path=config.ai.model_path,
-        n_gpu_layers=-1,
+        provides=OpenAI,
+        openai_api_key=config.ai.openai_api_key,
         temperature=0.3,
-        f16_kv=True,
-        verbose=False
+        verbose=True
     )
     chat_model = providers.Resource(
-        provides=Llama2Chat,
-        llm=llm,
-        temparature=0.3
+        provides=ChatOpenAI,
+        openai_api_key=config.ai.openai_api_key
     )
 
     ai_mutex = providers.Singleton(AiMutex, chat_model)
