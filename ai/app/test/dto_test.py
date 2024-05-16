e8f465970027e3beec972e260803d65c9ca94557
구본웅 <thesmartist3@gmail.com>
2024-05-16 12:19:10 +0900

    Reapply "Fix: to_redis name 가져오는 오류 수정"
    This reverts commit 2470155290bf7a2655b01111774e2242e2ce1e35.

--- ai/app/test/dto_test.py
+++ ai/app/test/dto_test.py
@@ -6,11 +6,17 @@ from redis import Redis
 from api.models.code import AnalysisStatus
 from api.models.dto import AnalysisDataDto, AiResultDto, CommitScoreDto
 
-
 original_dto = AnalysisDataDto(
-    analysis_id='0',
+    analysis_id='result:0',
     repo_path='DoubleDeltas/MineCollector',
+    project_id='0',
     user_name='DoubleDeltas',
+    member_id=9,
+    is_own=True,
+    percentage=0,
+    repo_start_date='2024-04-16',
+    repo_end_date='2024-05-16',
+    repo_member_cnt=1,
     result=AiResultDto(
         total_commit_cnt=100,
         personal_commit_cnt=10,
@@ -21,13 +27,12 @@ original_dto = AnalysisDataDto(
             performance=20,
             reusability=30,
             testability=40,
-            exception=50
+            exception=50,
+            score_comment="점수 코멘트"
         ),
-        lines_of_code={
-            3001: 100,
-            3002: 200
-        }
-    )
+    ),
+    status=AnalysisStatus.BEFORE_RECEIVING,
+    expire_sec=86400,
 )
 
 
@@ -39,7 +44,7 @@ if __name__ == '__main__':
     dto = original_dto
     dto.to_redis(redis_client, ex=60)
 
-    print(redis_client.get('0'))
+    print(redis_client.get('result:0'))
 
     # dto2 = AnalysisDataDto.from_redis(redis_client, '0')
     # print('TEST:', dto2.to_camel_dict())
