f94ecab790eb15f92025feacce361530fd83339c
신현중 <qsc3117@naver.com>
2024-05-16 11:44:37 +0900

    Design: 알림페이지

--- fe/src/app/alarm/page.tsx
+++ fe/src/app/alarm/page.tsx
@@ -24,30 +24,32 @@ export default function AlarmPage() {
     {},
   );
   return (
-    <main>
-      <h1 className="text-xl">알림 목록</h1>
-      <ul>
-        {Object.entries(groupedAlarmData).map(([date, alarms]) => (
-          <li key={date} className="p-1">
-            <p className="text-lg">{date}</p>
-            <ul className="flex flex-col gap-4 min-w-16">
-              {alarms.map((alarm, index) => (
-                <li key={index}>
-                  {alarm.repoViewTitle !== null ? (
-                    <p>
-                      {alarm.memberNickName}님이 {alarm.repoViewTitle}를
-                      조회했어요!
-                    </p>
-                  ) : (
-                    <p>{alarm.memberNickName}님이 팔로우했어요!</p>
-                  )}
-                  {/* <p>{alarm.createAt}</p> */}
-                </li>
-              ))}
-            </ul>
-          </li>
-        ))}
-      </ul>
+    <main className="bg-appGrey1 flex flex-col justify-center items-center">
+      <div className="max-w-screen-xl w-full flex flex-col py-4 gap-4">
+        <h1 className="text-xl">알림 목록</h1>
+        <ul>
+          {Object.entries(groupedAlarmData).map(([date, alarms]) => (
+            <li key={date} className="p-4 m-y-4 border-t-2">
+              <p className="text-xl">{date}</p>
+              <ul className="flex flex-col gap-2 min-w-16">
+                {alarms.map((alarm, index) => (
+                  <li key={index}>
+                    {alarm.repoViewTitle !== null ? (
+                      <p>
+                        {alarm.memberNickName}님이 {alarm.repoViewTitle}를
+                        조회했습니다
+                      </p>
+                    ) : (
+                      <p>{alarm.memberNickName}님이 팔로우했습니다</p>
+                    )}
+                    {/* <p>{alarm.createAt}</p> */}
+                  </li>
+                ))}
+              </ul>
+            </li>
+          ))}
+        </ul>
+      </div>
     </main>
   );
 }
