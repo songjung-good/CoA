2761b21e2bb5eb2d9772695c2914a9285544e6a3
신현중 <qsc3117@naver.com>
2024-05-18 16:10:16 +0900

    Fix: Buttons must have discernible text

--- fe/src/components/header/SearchBar.tsx
+++ fe/src/components/header/SearchBar.tsx
@@ -12,7 +12,7 @@ export default function SearchBar({ hasJWT }: { hasJWT: boolean }) {
 
   return (
     hasJWT && (
-      <button id='searchpage' aria-labelledby="searchpagebutton" onClick={handleButtonClick}>
+      <button id="searchpage" aria-label="검색하기" onClick={handleButtonClick}>
         <div className="w-5 h-5 md:w-7 md:h-7 lg:w-10 lg:h-10">
           <SearchIcon />
         </div>
