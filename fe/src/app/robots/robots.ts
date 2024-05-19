d2ffecb75149beff282c1896ffe543f1ddd4f69b
YH <139380475+songjung-good@users.noreply.github.com>
2024-05-19 19:07:55 +0900

    fix: 검색 정보 수정
    canonical 링크 추가: 중복 콘텐츠 이슈를 방지하고, 검색 엔진이 올바른 페이지 버전을 인식하도록 돕습니다.
    og:site_name 메타 태그 추가: 웹사이트의 이름을 제공합니다.

--- fe/src/app/robots/robots.ts
+++ fe/src/app/robots/robots.ts
@@ -4,23 +4,29 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
       {
+        userAgent: '*',
+        allow: ['/', '/main', '/search', '/robots.txt', '/sitemap.xml'],
+        disallow: ['/private/'],
+      },
+      {
         userAgent: 'Googlebot',
-        allow: ['/', '/main', '/search', '/main', '/robots.txt', '/sitemap.xml'],
+        allow: ['/', '/main', '/search', '/robots.txt', '/sitemap.xml'],
         disallow: ['/private/'],
       },
       {
         userAgent: 'Yeti',
-        allow: ['/', '/main', '/search', '/main', '/robots.txt', '/sitemap.xml'],
+        allow: ['/', '/main', '/search', '/robots.txt', '/sitemap.xml'],
         disallow: ['/private/'],
       },
       {
-        userAgent: ['Applebot', 'Bingbot'],
-        allow: ['/', '/main', '/search', '/main', '/robots.txt', '/sitemap.xml'],
+        userAgent: 'Applebot',
+        allow: ['/', '/main', '/search', '/robots.txt', '/sitemap.xml'],
         disallow: ['/private/'],
       },
       {
-        userAgent: '*',
-        disallow: '/',
+        userAgent: 'Bingbot',
+        allow: ['/', '/main', '/search', '/robots.txt', '/sitemap.xml'],
+        disallow: ['/private/'],
       },
     ],
     sitemap: 'https://commitanalyze.com/sitemap.xml',  // 사이트맵 URL을 수정
